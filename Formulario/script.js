ee2d944c649c3a04021a713eb13e0473cf8520d1
Laucha2102 <160553927+Laucha2102@users.noreply.github.com>
2025-09-25 01:12:59 -0300

    script.js

--- Formulario/script.js
+++ Formulario/script.js
@@ -1,34 +1,45 @@
 const submitFunction = (event) => {
-    event.prevetDefault() // Que se preventa la actualizacion de la web
-    validarFormulario()
-}
+    event.preventDefault(); // Prevenir actualización
+    validarFormulario();
+};
 
-Document.getElementById('formulario').addEventListener('submit', submitFunction) //escucha el envio del formulario 
+document.getElementById('formulario').addEventListener('submit', submitFunction);
 
-function validarFormulario(){
-    let camposTexto = document.querySelectorAll('input[type="text"]');
+function validarFormulario() {
+    const camposTexto = document.querySelectorAll('input[type="text"]');
     let validacionCorrecta = true;
 
     camposTexto.forEach(campo => {
-        let errorCampo = document.getElementById('error' + campo.id.charAt(0).toUpperCase() + campo.id.slice(1)) // error + id con la primera en mayuscula
-        if(campo.value.length == ''){
-            mostrarError (errorCampo, '¡Este campo es requerido!')
-            validacionCorrecta = false
-        }else if(campo.value.length > 0 && campo.value.length < 3){
-            mostrarError (errorCampo, '¡Este campo debe tener al menos 3 caracteres!')
-            validacionCorrecta = false
-        }else{
-            ocultarError(errorCampo)
+        let errorCampo = document.getElementById('error' + campo.id.charAt(0).toUpperCase() + campo.id.slice(1));
+        
+        if (campo.value.length === 0) {
+            mostrarError(errorCampo, '¡Este campo es requerido!');
+            validacionCorrecta = false;
+        } else if (campo.value.length < 3) {
+            mostrarError(errorCampo, '¡Este campo debe tener al menos 3 caracteres!');
+            validacionCorrecta = false;
+        } else {
+            ocultarError(errorCampo);
         }
-    })
+    });
+
+    const email= document.getElementById('email');
+    let errorEmail = document.getElementById('errorEmail')
+
+    if(/^[^\s@]+@{^\s@}+\.{^\s@}+$/.test(email.value)){
+        ocultarError(errorEmail)
+    }else{
+        mostrarError(errorEmail, 'Ingrese un correo electronico valido!')
+    }
+
 }
 
-const mostrarError = (elemento, mensaje) =>{
+const mostrarError = (elemento, mensaje) => {
     elemento.textContent = mensaje;
     elemento.style.display = "block";
-}
+};
 
-const ocultarError = (elemento) =>{
+const ocultarError = (elemento) => {
     elemento.textContent = '';
     elemento.style.display = "none";
-}
+};
